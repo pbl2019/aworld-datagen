0330f4e2ddf187cd768e64d0d8872459aa1016a1
tamamu <tamamu.1r1s@gmail.com>
2019-10-11 17:27:37 +0900

    Add define-enum macro

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ extern crate dotenv;
 
 pub mod models;
 pub mod schema;
+pub mod utils;
---
+++ src/utils.rs
@@ -0,0 +1,46 @@
+#[macro_export]
+macro_rules! define_enum {
+    (
+        $(#[$meta:meta])*
+        pub enum $name:ident { $($variant:ident = $val:expr,)* }
+    ) => {
+        use diesel::sql_types::Integer;
+        use diesel::serialize::ToSql;
+        use diesel::deserialize::FromSql;
+
+        // 元の enum を必要な derive とともに定義
+        $(#[$meta])*
+        #[derive(FromSqlRow, AsExpression)]
+        #[sql_type = "Integer"]
+        pub enum $name {
+            $($variant = $val,)*
+        }
+
+        // `ToSql`を定義
+        impl<DB: diesel::backend::Backend> ToSql<Integer, DB> for $name {
+            fn to_sql<W: std::io::Write>(
+                &self,
+                out: &mut diesel::serialize::Output<W, DB>,
+            ) -> Result<diesel::serialize::IsNull, Box<std::error::Error + Send + Sync>> {
+                ToSql::<Integer, DB>::to_sql(&(*self as i32), out)
+            }
+        }
+
+        // `FromSql`を定義
+        impl<DB: diesel::backend::Backend> FromSql<Integer, DB> for $name
+        where
+            i32: FromSql<Integer, DB>,
+        {
+            fn from_sql(
+                bytes: Option<&DB::RawValue>,
+            ) -> Result<Self, Box<std::error::Error + Send + Sync>> {
+                use self::$name::*;
+
+                match <i32 as FromSql<Integer, DB>>::from_sql(bytes)? {
+                    $($val => Ok($variant),)*
+                    s => Err(format!("invalid {} value: {}", stringify!($name), s).into()),
+                }
+            }
+        }
+    }
+}
