1deafcf4d62adc999889b1e2ac2867d2c747a3d3
S-Kaito <irc.darjeeling@gmail.com>
2019-10-11 18:15:10 +0900

    fix base64
    merge master

--- src/models/terrain.rs
+++ src/models/terrain.rs
@@ -1,21 +1,29 @@
 use crate::schema::terrains;
+extern crate base64;
+use base64::{encode, decode};
 
 #[derive(Queryable)]
 pub struct Terrain {
 	pub id: i64,
 	pub content: String,
+	pub width: i32,
+	pub height: i32,
 }
 
 #[derive(Insertable)]
 #[table_name = "terrains"]
 pub struct NewTerrain {
-	pub content: String,
+	pub content: String ,
+	pub width: i32,
+	pub height: i32,
 }
 
 impl std::default::Default for NewTerrain {
 	fn default() -> Self {
 		Self {
-			content: "00000000000000000000000000".to_string(),
+			content: encode(&vec![0u8; 10000]),
+			width: 100,
+			height: 100,
 		}
 	}
 }
