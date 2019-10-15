71a0cd4cad4c54aa5863f3c1b25bd0398739a541
S-Kaito <irc.darjeeling@gmail.com>
2019-10-15 16:53:53 +0900

    add terrain

--- src/models/terrain.rs
+++ src/models/terrain.rs
@@ -1,6 +1,7 @@
 use crate::schema::terrains;
 extern crate base64;
 use base64::{encode, decode};
+use rand::Rng;
 
 #[derive(Queryable)]
 pub struct Terrain {
@@ -20,8 +21,29 @@ pub struct NewTerrain {
 
 impl std::default::Default for NewTerrain {
 	fn default() -> Self {
+		let mut content = "".to_string();
+		for i in 0..500 {
+			content += "#";
+		}
+		let mut rng = rand::thread_rng();
+		for i in 5..95{
+			content += "#####";
+			for j in 5..95{
+				let a: i32 = rng.gen();
+				if (a % 2 == 0){
+					content += " ";
+				}else{
+					content += "#";
+				}
+			}
+			content += "#####";
+		}
+		for i in 0..500 {
+			content += "#";
+		}
 		Self {
-			content: encode(&vec![0u8; 10000]),
+			//content: encode(&vec![0u8; 10000]),
+			content: content,
 			width: 100,
 			height: 100,
 		}
@@ -32,4 +54,10 @@ impl std::default::Default for NewTerrain {
 fn create_character() {
 	let new_terrain = NewTerrain::default();
 	assert!(new_terrain.content.len() > 0);
+	for i in 0..100{
+		for j in 0..100{
+			print!("{}",new_terrain.content.chars().nth(i * 100 + j).unwrap())
+		}
+		println!("")
+	}
 }
