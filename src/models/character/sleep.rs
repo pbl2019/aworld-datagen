07e685ed0f6567092468d9b58ec77699d1f50752
tomatoaiu <boushi_sukaimu@yahoo.co.jp>
2019-10-18 08:51:19 +0000

    feat(character): add sleep file

---
+++ src/models/character/sleep.rs
@@ -0,0 +1,12 @@
+impl Character {
+    pub fn sleep(time_ms: i32) {
+        // 眠っている間は機能が制限される
+        // 眠りの深さを超えなかったら動かない
+        // 眠りの深さ
+        // sleep_start, sleep_endみたいなメソッドを生やして、CharacterLocal?
+        // 前回の動いた時刻　眠った時間
+
+        // 眠っている間におきるイベントをcronみたいな感じで実行できるよ良い?
+        // time durationでも?
+    }
+}
