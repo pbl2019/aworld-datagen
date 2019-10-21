99d61ce71f4127ee66d4e37d8a4ef1a3bbf4ee03
amaisuna <taro30143014@gmail.com>
2019-10-21 19:03:46 +0900

    move angle

--- src/models/character/move.rs
+++ src/models/character/move.rs
@@ -1,8 +1,13 @@
 use crate::models::character::CharacterLocal;
+use std::f32;
 
 impl CharacterLocal {
     fn CharacterLocal::move_forward(&mut self) {
-        self.
+        self.x += self.angle.cos();
+        self.y += self.angle.sin();
     }
-    fn CharacterLocal::move_back() {}
+    fn CharacterLocal::move_back(&mut self) {
+        self.x -= self.angle.cos();
+        self.y -= self.angle.sin();
+    }
 }
