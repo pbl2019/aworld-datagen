3f8f6ab2ba362b5d272d123b3c2175903785631e
amaisuna <taro30143014@gmail.com>
2019-10-25 17:19:00 +0900

    feature/action/ move

--- src/actions/character_action.rs
+++ src/actions/character_action.rs
@@ -1,3 +1,10 @@
+pub enum CharacterAction {
+    Forward { speed: f32 },
+    Back { speed: f32 },
+    TurnLeft { angle: f32 },
+    TurnRight { angle: f32 },
+}
+
 #[derive(Debug)]
 pub enum CharacterEffect {
     Damage { amount: i32 },
