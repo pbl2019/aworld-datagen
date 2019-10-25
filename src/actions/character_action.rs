fe9f0cc5f200ae01ca932c33621dcba6a8cd2e36
S-Kaito <irc.darjeeling@gmail.com>
2019-10-25 17:27:09 +0900

    fix amount

--- src/actions/character_action.rs
+++ src/actions/character_action.rs
@@ -1,12 +1,12 @@
 #[derive(Debug)]
 pub enum CharacterEffect {
     Damage { amount: i32 },
-    Die { amount : i32 },
+    Dead {},
 }
 
 pub enum CharacterAction {
-    Attack { amount: i32 },
-    Use { amount: i32 },
-    Defence { amount: i32 },
-    Avoidance { amount: i32 },
+    Attack {},
+    Use {itemId: String},
+    Defence {},
+    Avoid{},
 }
