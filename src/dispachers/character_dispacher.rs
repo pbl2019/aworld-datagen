5e7dcf81e0af3cd2c06b76994ddc01c31826a0a1
S-Kaito <irc.darjeeling@gmail.com>
2019-10-25 18:13:35 +0900

    add characterDisptch

---
+++ src/dispachers/character_dispacher.rs
@@ -0,0 +1,9 @@
+src/dispatchers/character_dispatcher.rs
+
+struct CharacterDispatcher;
+impl CharacterDispatcher {
+    fn effect_damage(store: &mut CharacterLocal, payload: &CharacterDamagePoyload) -> Result<()> {
+        store.update_hp(store.hp - payload.amount);
+        Ok(())
+    }
+}
