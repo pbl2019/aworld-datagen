6044905ec76ce74bd4f078a4c057438a8dd1b295
S-Kaito <irc.darjeeling@gmail.com>
2019-10-25 17:12:19 +0900

    add actions

--- src/actions/character_action.rs
+++ src/actions/character_action.rs
@@ -1,4 +1,12 @@
 #[derive(Debug)]
 pub enum CharacterEffect {
     Damage { amount: i32 },
+    Die { amount : i32 },
+}
+
+pub enum CharacterAction {
+    Attack { amount: i32 },
+    Use { amount: i32 },
+    Defence { amount: i32 },
+    Avoidance { amount: i32 },
 }
