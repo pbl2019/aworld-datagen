a6e5fde72ff69763bf44f6e1fb1a1a8601157442
tomatoaiu <boushi_sukaimu@yahoo.co.jp>
2019-10-25 17:17:20 +0900

    add sleep action

--- src/actions/character_action.rs
+++ src/actions/character_action.rs
@@ -1,4 +1,15 @@
 #[derive(Debug)]
+pub enum CharacterAction {
+    Sleep { amount: i32 },
+    Rest { amount: i32 },
+    Idle { amount: i32 },
+    GetUp { amount: i32 },
+}
+
+#[derive(Debug)]
 pub enum CharacterEffect {
     Damage { amount: i32 },
+    Heal { amount: i32 },
+    WakeUp { amount: i32 }
+    WokenUp { amount: i32 }
 }
