7a0781ef47614dbf8959790eb4060da77378e970
S-Kaito <irc.darjeeling@gmail.com>
2019-10-29 17:45:18 +0900

    fix pOyload

--- src/dispatches/character_dispatcher.rs
+++ src/dispatches/character_dispatcher.rs
@@ -1,13 +1,13 @@
 struct CharacterDispatcher;
 impl CharacterDispatcher {
-    fn effect_damage(store: &mut CharacterLocal, payload: &CharacterDamagePoyload) -> Result<()> {
+    fn effect_damage(store: &mut CharacterLocal, payload: &CharacterDamagePayload) -> Result<()> {
         store.update_hp(store.hp - payload.amount);
         if store.hp - payload.amount <= 0{
             store.update_dead(true);
         }
         Ok(())
     }
-    fn effect_dead(store: &mut CharacterLocal, payload: &CharacterDamagePoyload) -> Result<()> {
+    fn effect_dead(store: &mut CharacterLocal, payload: &CharacterDamagePayload) -> Result<()> {
     	store.update_dead(true);
         Ok(())
     
--- src/dispatches/item_dispatcher.rs
+++ src/dispatches/item_dispatcher.rs
@@ -1,10 +1,10 @@
 struct ItemDispatcher;
 impl ItemDispatcher {
-	fn effect_spend(store: &mut ItemLocal, payload: &ItemDamagePoyload) -> Result<()> {
+	fn effect_spend(store: &mut ItemLocal, payload: &ItemDamagePayload) -> Result<()> {
 		store.update_is_used(true);
 		Ok(())
 	}
-	fn effect_break(store: &mut ItemLocal, payload: &ItemDamagePoyload) -> Result<()> {
+	fn effect_break(store: &mut ItemLocal, payload: &ItemDamagePayload) -> Result<()> {
 			store.update_amount(store.amount - payload.amount);
 		if store.amount - payload.amount <= 0{
 			store.update_is_used(true);
