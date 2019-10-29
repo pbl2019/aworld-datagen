305e753155bc581e3ece2686148ac394618f1561
amaisuna <taro30143014@gmail.com>
2019-10-29 17:20:52 +0900

    Add move dispatcher in CharacterDispatcher

---
+++ src/dispatchers/character_dispatcher.rs
@@ -0,0 +1,23 @@
+struct CharacterDispatcher;
+impl CharacterDispatcher {
+    fn forward(store: &mut CharacterLocal, payload: &CharacterForwardPayload) -> Result<()> {
+        store.update_pos(store.x + store.angle.sin() * payload.speed, store.y + store.angle.cos() * payload.speed);
+        Ok(())
+    }
+    fn backward(store: &mut CharacterLocal, payload: &CharacterBackwardPayload) -> Result<()> {
+        store.update_pos(store.x - store.angle.sin() * payload.speed, store.y - store.angle.cos() * payload.speed);
+        Ok(())
+    }
+    fn turn_left(store: &mut CharacterLocal, payload: &CharacterTurnLeftPayload) -> Result<()> {
+        store.update_angle(store.angle - payload.angle);
+        Ok(())
+    }
+    fn turn_right(store: &mut CharacterLocal, payload: &CharacterTurnRightPayload) -> Result<()> {
+        store.update_angle(store.angle + payload.angle);
+        Ok(())
+    }
+    fn effect_pushed(store: &mut CharacterLocal, payload: &CharacterPushedPayload) -> Result<()> {
+        store.update_pos(store.x + payload.angle.sin(), store.y + payload.angle.cos());
+        Ok(())
+    }
+}
