a81de844039e9e437bf5bb5817d97c44a47daf44
tamamu <tamamu.1r1s@gmail.com>
2019-11-01 11:35:43 +0900

    Fix

--- src/dispatchers/character_dispatcher.rs
+++ src/dispatchers/character_dispatcher.rs
@@ -1,15 +1,18 @@
+use crate::actions::*;
+use crate::models::character_action::*;
+
 struct CharacterDispatcher;
 impl CharacterDispatcher {
     fn effect_damage(store: &mut CharacterLocal, payload: &CharacterDamagePayload) -> Result<()> {
-        store.update_hp(store.hp - payload.amount);
-        if store.hp - payload.amount <= 0{
-            store.update_dead(true);
+        let mut hp = store.hp.read() - payload.amount;
+        store.hp.write(hp);
+        if hp <= 0 {
+            store.dead.write(true);
         }
         Ok(())
     }
     fn effect_dead(store: &mut CharacterLocal, payload: &CharacterDamagePayload) -> Result<()> {
-    	store.update_dead(true);
+        store.dead.write(true);
         Ok(())
-    
     }
 }
