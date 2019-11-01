685c20c59b9f06985d6995ac8340a215d108f7f4
tamamu <tamamu.1r1s@gmail.com>
2019-11-01 12:34:18 +0900

    Fix

--- src/models/terrain.rs
+++ src/models/terrain.rs
@@ -1,15 +1,15 @@
 use crate::init_field;
 use crate::models::field::Field;
 use crate::schema::terrains;
-use base64::{decode, encode};
+use base64;
 use rand::Rng;
 
 #[derive(Queryable, Clone)]
 pub struct Terrain {
     pub id: i64,
     pub content: String,
-    pub width: i32,
-    pub height: i32,
+    pub width: u32,
+    pub height: u32,
 }
 
 #[derive(Insertable)]
@@ -22,43 +22,33 @@ pub struct NewTerrain {
 
 pub struct TerrainLocal {
     pub model: Terrain,
+    pub raw: Field<Vec<u8>>,
 }
 
 impl std::default::Default for NewTerrain {
     fn default() -> Self {
-        let mut content = "".to_string();
-        for i in 0..500 {
-            content += "#";
-        }
         let mut rng = rand::thread_rng();
-        for i in 5..95 {
-            content += "#####";
-            for j in 5..95 {
-                let a: i32 = rng.gen();
-                if (a % 2 == 0) {
-                    content += " ";
-                } else {
-                    content += "#";
-                }
-            }
-            content += "#####";
-        }
-        for i in 0..500 {
-            content += "#";
+        let width = rng.gen_range(50, 100);
+        let height = rng.gen_range(50, 100);
+        let mut raw = Vec::with_capacity(width * height);
+        for j in 0..width * height {
+            raw.push(rng.gen_range(0, 1 + 1));
         }
         Self {
-            //content: encode(&vec![0u8; 10000]),
-            content: content,
-            width: 100,
-            height: 100,
+            content: base64::encode(&raw),
+            width: width as i32,
+            height: height as i32,
         }
     }
 }
 
 impl std::convert::From<Terrain> for TerrainLocal {
     fn from(model: Terrain) -> Self {
+        let raw = base64::decode(&model.content).unwrap();
         Self {
             model: model.clone(),
+
+            raw: init_field!(raw),
         }
     }
 }
@@ -67,10 +57,12 @@ impl std::convert::From<Terrain> for TerrainLocal {
 fn create_character() {
     let new_terrain = NewTerrain::default();
     assert!(new_terrain.content.len() > 0);
-    for i in 0..100 {
-        for j in 0..100 {
-            print!("{}", new_terrain.content.chars().nth(i * 100 + j).unwrap())
+    let raw = base64::decode(&new_terrain.content).unwrap();
+    assert_eq!(raw.len(), (new_terrain.width * new_terrain.height) as usize);
+    for i in 0..new_terrain.height {
+        for j in 0..new_terrain.width {
+            print!("{}", raw[(i * new_terrain.width + j) as usize])
         }
-        println!("")
+        print!("\n");
     }
 }
