a867910b9c88b541065875b352a3ff7b869a4513
S-Kaito <irc.darjeeling@gmail.com>
2019-11-05 17:32:24 +0900

    for save

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -1,1 +1,2 @@
 pub mod character_action;
+pub mod relation_action;
--- src/dispatchers/relation_dispatcher.rs
+++ src/dispatchers/relation_dispatcher.rs
@@ -13,4 +13,19 @@ impl RelationDispatcher {
 #[test]
 fn test_dispatch(){
 	dispatcher = &RelationDispatcher;
+	let relation = Relation{id:1, character_id:1, target_id:2, factor: 0.25};
+	let mut rl = RelationLocal::from(relation);
+	let mut pl = RelationIncreasePayloac({amout: 32})
+
+	let res = RelationDispatcher::effect_increase(rl,pl)
+	assert!(res.is_ok())
+
+	// let charaAlpha = Character { id: 001, name: "tset".to_string(), max_hp: 100, max_appetite: 200 };
+ //    let mut character = CharacterLocal::from(mock);
+ //    let sleep_state = Sleeping { state: SleepingState::GettingUp, depth: 100 };
+ //    character.sleep_state.write(sleep_state);
+ //    let res = CharacterDispatcher::action_sleep(&mut character);
+ //    assert!(res.is_ok());
+ //    assert!(character.sleep_state.read().state == SleepingState::Sleeping);
+ //    assert!(character.sleep_state.read().depth == MAX_SLEEP_AMOUNT);
 }
