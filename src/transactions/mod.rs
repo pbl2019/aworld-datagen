ce3539255b4d30bd7c8b0729119a1423ab53aa70
tamamu <tamamu.1r1s@gmail.com>
2019-11-10 18:20:38 +0900

    Remove dead code

--- src/transactions/mod.rs
+++ src/transactions/mod.rs
@@ -1,8 +1,1 @@
 pub mod forward;
-
-use crate::connection::Connection;
-use crate::context::Context;
-
-pub trait Transaction {
-    fn call(&self, conn: &Connection, context: &Context) -> Result<Vec<i64>, ()>;
-}
